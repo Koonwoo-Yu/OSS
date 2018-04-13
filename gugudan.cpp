45eb1b846e842eb37515b32baf67b9ea39cc49b9
Koonwoo Yu <gunuboard@gmail.com>
2018-04-13 10:03:18 +0900

    change gugudan.cpp

--- gugudan.cpp
+++ gugudan.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main() {
-	for (int i = 2; i <= 8; i++) {
+	for (int i = 2; i <= 9; i++) {
 		cout << i << "´Ü" << endl;
 		for (int j = 1; j <= 8; j++) {
 			cout << i << " * " << j << " = " << i*j << endl;
